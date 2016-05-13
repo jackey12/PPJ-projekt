b7bc479a936728a92542d030509c65e088dacc33
Jakub Hirnšal <Jakub Hirnal>
2016-05-13 14:15:17 +0200

    first commit

---
+++ src/main/java/com/semestral/hirnsal/SemestralJhApplication.java
@@ -0,0 +1,12 @@
+package com.semestral.hirnsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralJhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralJhApplication.class, args);
+	}
+}
---
+++ src/test/java/com/semestral/hirnsal/SemestralJhApplicationTests.java
@@ -0,0 +1,18 @@
+package com.semestral.hirnsal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SemestralJhApplication.class)
+@WebAppConfiguration
+public class SemestralJhApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
